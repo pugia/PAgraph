640bc2c27f69bf0b9d6f9de10632eaa73036bd62
Marco Pugliese <marco.pugliese@pugia.com>
2015-09-08 18:11:01 +0200

    Edit compare method and begin weekday mode

--- src/js/PAgraph.js
+++ src/js/PAgraph.js
@@ -38,6 +38,7 @@
 		var structure = {
 			legend: null,
 			tooltip: null,
+			data: [null,null],
 			svg: {
 				element: null,
 				grid: {
@@ -272,6 +273,55 @@
 										
 					return index;
 
+				},
+				
+				mainGraph: function(data) {
+					
+					var self = this;
+					var index = 0;
+					
+					if (data == undefined) { var data = structure.data[0]; }
+					if (data.length == 0) return;
+					
+					structure.data[0] = data;
+					var allData = mergeAndClean(structure.data);
+					structure.svg.grid.y.spacing = Yspacing(allData);
+
+					if (data.length != structure.svg.grid.x.elements.length) {
+						if (structure.data[1]) { self.removeCompareGraph() }
+						structure.svg.grid.y.spacing = Yspacing(data);
+						self.flattenGraph(data, 0, function() {
+							self.drawGraphAnimation(structure.data[0], 0);
+						})	
+					} else {
+						self.drawGraphAnimation(structure.data[0], 0);
+						if (structure.data[1]) { self.drawGraphAnimation(structure.data[1], 1); }
+					}
+					
+				},
+				
+				compareWith: function(data) {
+					
+					var self = this;
+					var index = structure.svg.graph.elements.length == 1 ? self.initGraph() : 1;
+					
+					if (data == undefined) { var data = structure.data[index]; }
+					if (data.length == 0) return;
+
+					structure.data[index] = data;
+					var allData = mergeAndClean(structure.data);
+					structure.svg.grid.y.spacing = Yspacing(allData);
+					
+					if (data.length != structure.svg.grid.x.elements.length) {
+						self.flattenGraph(data, index, function() {
+							self.drawGraphAnimation(structure.data[0], 0);
+							self.drawGraphAnimation(structure.data[1], 1);
+						})	
+					} else {
+						self.drawGraphAnimation(structure.data[0], 0);
+						self.drawGraphAnimation(structure.data[1], 1);
+					}
+					
 				},
 				
 				/* ANIMATE */
@@ -692,6 +742,338 @@
 				},
 				
 				// remove last graph
+				removeCompareGraph: function() {
+					
+					var self = this;
+					self.flattenGraph([], 1, function() {
+						structure.svg.graph.elements[1].group.remove();
+						structure.svg.graph.elements.pop();
+						graph.find('div.PAlegend > p[data-index=1]').remove()
+						structure.data[1] = null;
+					});
+					
+				},
+				
+				removeCompare: function() {
+					
+					var self = this;
+					self.removeCompareGraph();
+					setTimeout(function() {
+						self.mainGraph();
+					}, internalSettings.graphAnimationTime+2);
+					
+				},
+				
+				// action on legend
+				initLegend: function() {
+					
+					var self = this;
+					
+					graph.find('div.PAlegend')	
+						.on('click', 'p[data-index]', function() {
+							var index = parseInt($(this).attr('data-index'));
+							self.moveOnFront(index);
+						})
+					
+					
+				},
+				
+				initCircles: function() {
+					var self = this;
+					
+					graph
+						.on('mouseover', 'circle', function() {
+							
+							structure.tooltip.style('display', 'block');
+
+							// label and value
+							var index = $(this).parent('g').attr('data-index');
+							
+							structure.tooltip
+								.style('color', settings.config.graph[index].color)
+								.select('span').text($(this).attr('data-value'));
+
+							structure.tooltip
+								.select('label').text(settings.config.graph[index].legend);
+								
+							// set tooltip position
+							var size = structure.tooltip.node().getBoundingClientRect();
+							
+							var t = $(structure.tooltip[0][0]),
+									w = t.width(),
+									h = t.height()
+							
+							var px = $(this).attr('cx'),
+									py = $(this).attr('cy')
+									
+							var top = py - h - 20,
+									left= px - parseInt(w/2)
+							
+							t.css('top', top)
+							 .css('left',left)
+							 .addClass('show')
+							
+						})
+						.on('mouseout', 'circle', function() {
+							structure.tooltip.classed('show', false);
+							setTimeout(function() {
+								structure.tooltip.style('display', 'none');
+							}, 300)
+						})
+					
+				},
+				
+				// move the selected graph on front
+				moveOnFront: function(index) {
+					
+					var self = this;
+					var g = graph.find('g.PAGgraphs');
+					g.find('g.PAGgraph[data-index='+index+']').appendTo(g);
+					
+				},
+							
+			},
+			
+			weekday: {
+											
+				// start with a week structure
+				init: function() {
+					
+					debug('MODE: weekday');
+					
+					var self = this;
+					
+					self.grid();
+					
+				},
+				
+				grid: function() {
+					
+					var self = this;
+					
+					self.initGridY();
+					self.initGraph();
+					self.initLegend();
+					self.initCircles();
+					
+				},
+				
+				/* INIT */
+				// create empty grid without label
+				initGridY: function() {
+					var self = this;
+					
+					var w = graph.width();
+					var h = graph.height();
+					var j = 0;
+					
+					if (settings.config.grid.x.label != false) { h = h - internalSettings.labels.x.height; }
+					if (settings.config.grid.y.label != false) {  j = internalSettings.labels.y.width; w = w - j; }
+					
+					var elementsCount = 6;
+					
+					var spacing = Math.floor(h / elementsCount);
+					structure.svg.grid.y.group = structure.svg.grid.group.append('g').classed('PAGgridY', true);		
+					
+					
+					for (i = 0; i < elementsCount; i++) {
+						
+						var line =  structure.svg.grid.y.group.append('line')
+																									.attr('x1', j).attr('y1', h - (i*spacing))
+																									.attr('x2', graph.width()).attr('y2', h - (i*spacing))
+																									.attr('stroke', settings.config.grid.y.color)
+						structure.svg.grid.y.elements.push(line);
+														
+					}
+					
+				},
+			
+				// create flat graph
+				initGraph: function() {
+					
+					var self = this;
+										
+					var w = graph.width();
+					var h = graph.height();
+					var j = 0;
+					
+					var graphElements = $.extend(true, {}, graphElementsEmpty);
+					
+					graphElements.group = structure.svg.graph.group.insert('g',':first-child')
+																							 					 .classed('PAGgraph', true)
+					graphElements.elements.area = [];
+					structure.svg.graph.elements.push(graphElements);
+										
+					var index = structure.svg.graph.elements.length - 1;
+					structure.svg.graph.elements[index].group.attr('data-index', index);
+					
+					if (settings.config.grid.x.label != false) { h = h - internalSettings.labels.x.height; }
+					if (settings.config.grid.y.label != false) {  j = internalSettings.labels.y.width; w = w - j; }
+					var spacing = Math.floor(w/7);
+					
+					var rectW = Math.floor(spacing*0.7);
+					var offsetX = Math.floor(spacing*0.1);
+
+					for (i = 0; i < 7; i++) {
+						
+						var x = (i * spacing) + j + (spacing / 2) - (rectW / 2) + (index * offsetX);
+
+						var rect = structure.svg.graph.elements[index].group.append('rect')
+																																.attr('x', x)
+																																.attr('y', h-1)
+																																.attr('width', rectW)
+																																.attr('height', 1)
+																																.attr('rx', 5)
+																																.attr('ry', 5)
+																																.attr('fill', settings.config.graph[index].color)
+						structure.svg.graph.elements[index].elements.area.push(rect);
+						
+					}					
+
+					// add label to legend
+					var p = structure.legend.append('p')
+																	.attr('data-index', index)
+					p.append('span')
+					 .style('background', settings.config.graph[index].color);
+					p.append('label')
+					 .text(settings.config.graph[index].legend);
+										
+					return index;
+
+				},
+				
+				/* ANIMATE */
+				// draw graph data
+				drawGraph: function(data, index) {
+					
+					var self = this;
+					if (data.length == 0) return;
+					
+					structure.data[index] = data;
+
+					var allData = (structure.svg.graph.elements.length > 1) ? structure.data[0].concat(structure.data[1]) : data;					
+					structure.svg.grid.y.spacing = Yspacing(allData, 5);
+					self.animateLabelY(allData);
+					
+					setTimeout(function() {
+											
+						self.animateGraph(structure.data[0], 0);
+						if (structure.svg.graph.elements.length > 1) {
+							self.animateGraph(structure.data[1], 1);
+						}
+						
+					}, internalSettings.animateGridTime);										
+					
+				},
+								
+				// fix the number of labels and animate
+				// remove the labels outside the artboard
+				animateLabelY: function(data) {
+					var self = this;
+					
+					var w = graph.width();
+					var h = graph.height();
+					var j = 0;
+					
+					var elementsCount = data.length;
+
+					if (settings.config.grid.x.label) { h = h - internalSettings.labels.x.height; }
+					if (settings.config.grid.y.label) {  j = internalSettings.labels.y.width; w = w - j; }
+					
+					var elementsCount = 6;
+					var spacing = Math.floor(h / elementsCount);
+					
+					// hide current labels
+					structure.svg.label.y.group.classed('hide', true);
+					
+					// fix the number of labels and position
+					setTimeout(function() {
+						for (i = 0; i < elementsCount; i++) {
+							
+							if (structure.svg.label.y.elements[i]) { // move existing and update text
+
+								structure.svg.label.y.elements[i].text(structure.svg.grid.y.spacing[i])
+
+							} else {
+								var label =  structure.svg.label.y.group.append('text')
+																	.attr('x', 50)
+																	.attr('y', h - (i*spacing))
+																	.attr('text-anchor','end')
+																	.attr('fill', settings.config.grid.y.label)
+																	.text(structure.svg.grid.y.spacing[i])
+								structure.svg.label.y.elements.push(label);
+							}
+															
+						}
+						
+					}, internalSettings.animateGridTime);
+										
+					setTimeout(function() {
+						// show current labels
+						structure.svg.label.y.group.classed('hide', false);
+					}, internalSettings.animateGridTime*2)
+								
+				},
+				
+				// animate line and area
+				animateGraph: function(data, index) {
+					var self = this;
+					
+					var index = index || 0;
+					
+					var w = graph.width();
+					var h = graph.height();
+					var j = 0;
+					
+					if (settings.config.grid.x.label != false) { h = h - internalSettings.labels.x.height; }
+					if (settings.config.grid.y.label != false) {  j = internalSettings.labels.y.width; w = w - j; }
+
+					var spacingX = Math.floor(w / (data.length - 1) );
+					var spacingY = (Math.floor(h / 6)) / (structure.svg.grid.y.spacing[1] - structure.svg.grid.y.spacing[0]);
+					
+					for (i in structure.svg.graph.elements[index].elements.area) {
+						
+						var y = parseInt(h - ((data[i].value - structure.svg.grid.y.spacing[0])  * spacingY ));						
+						
+						structure.svg.graph.elements[index].elements.area[i].transition()
+																			    												.duration(internalSettings.graphAnimationTime)
+																			    												.attr('y', y)
+																			    												.attr('height', h-y)
+																			    												.ease(internalSettings.animateEasing)					
+					}
+										
+				},
+				
+				// comparison
+				compareWith: function(data) {
+														
+					var self = this;
+					var index = (structure.svg.graph.elements.length == 1) ? self.initGraph() : 1;
+					
+					structure.data[index] = data;
+					
+					var allData = structure.data[0].concat(structure.data[1]);
+					structure.svg.grid.y.spacing = Yspacing(allData, 5);
+					
+					self.animateLabelY(allData);
+					
+					self.animateGraph(structure.data[0], 0);
+					self.animateGraph(structure.data[1], 1);
+					
+				},
+				
+								
+				// add a graph in back
+				addGraph: function(data) {
+					
+					var self = this;
+										
+					var index = self.initGraph();
+					self.animateGraph(data, index);
+					
+				},
+				
+				// remove last graph
 				removeGraph: function() {
 					
 					var self = this;
@@ -785,15 +1167,27 @@
 			
 		};
 		
+		self.mainGraph = function(data) {
+			
+			MODE[settings.mode].mainGraph(data);
+			
+		};
+		
+		self.compareWith = function(data) {
+			
+			MODE[settings.mode].compareWith(data);
+			
+		};
+		
 		self.addGraph = function(data) {
 
 			MODE[settings.mode].addGraph(data);
 
 		}
 		
-		self.removeGraph = function() {
+		self.removeCompare = function() {
 			
-			MODE[settings.mode].removeGraph();
+			MODE[settings.mode].removeCompare();
 			
 		}
 		
@@ -801,9 +1195,9 @@
 			return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 		}
 						
-		function Yspacing(data) {
+		function Yspacing(data, lines) {
 			
-			var lines = 5;
+			var lines = lines || 5;
 			
 			var max = getMaxValues(data);
 			var min = getMinValues(data);
@@ -826,6 +1220,20 @@
 			
 			return labels;
 						
+		}
+		
+		function mergeAndClean(arr) {
+						
+			var result = [];
+			for (i in arr) {
+				for (x in arr[i]) {
+					result.push(arr[i][x]);
+				}
+			}
+			
+			
+			return result;
+			
 		}
 		
 		function getMaxValues(data, k) {
