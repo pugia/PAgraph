c725f17e1c6a9baef5c7aa05f5fbe2900ceb641b
Taylor Case <tcase@cloud4wi.com>
2016-02-11 14:28:48 -0800

    added gender dist without pie chart

--- src/js/PAgraph.js
+++ src/js/PAgraph.js
@@ -1915,7 +1915,6 @@
 			slice.on('mouseover', function() {
 				var mouseover_object = arguments[0];
 				var index = arguments[1];
-				console.log(mouseover_object);
 
 				structure.tooltip.html(
 					'<div>' + mouseover_object.data.label + ': ' + mouseover_object.data.value + '</div>'
@@ -2001,7 +2000,6 @@
 					y: h / 2
 				};
 
-				console.log(center);
 				var damper = .1;
 
 				var max_amount = d3.max(data, function(d) {
@@ -2413,6 +2411,49 @@
 
 			},
 
+			gender_distribution_counters: {
+				female_icon: '<svg viewBox="0 0 58 58"><g fill="none"><rect fill="#808F96" x="0" y="0" width="58" height="58" rx="40"></rect><circle stroke="#FFFFFF" stroke-width="2" cx="29" cy="29" r="26.88"></circle><path d="M34.52496,40.79136 C34.49584,40.46992 34.47456,39.9536 34.45776,39.41824 C39.32416,38.91984 42.73792,37.7528 42.73792,36.39312 C42.72448,36.39088 42.7256,36.33712 42.7256,36.31472 C39.08784,33.03648 45.87952,9.73936 33.23584,10.212 C32.44176,9.54 31.05184,8.94304 29.05824,8.94304 C11.93232,10.23888 19.50464,32.23904 15.60256,36.392 C15.60032,36.39312 15.59696,36.39312 15.59472,36.39312 C15.59472,36.39536 15.59584,36.3976 15.59584,36.39984 C15.59584,36.40096 15.59472,36.40208 15.59472,36.40208 C15.59472,36.40208 15.59584,36.40208 15.59696,36.4032 C15.61264,37.73488 18.91104,38.88064 23.63632,39.39136 C23.62288,39.71616 23.59488,40.11824 23.53328,40.79136 C22.09744,44.652 14.62032,44.89392 10.4752,48.7344 C12.74096,50.71232 20.63584,56.02336 29.10528,56.02336 C37.57472,56.02336 44.60832,52.00368 47.4128,48.6224 C43.25872,44.89056 35.94624,44.61392 34.52496,40.79136 L34.52496,40.79136 Z" fill="#FFFFFF"></path></g></svg>',
+				male_icon: '<svg viewBox="0 0 58 58"><g fill="none"><rect fill="#88b8c4" x="0" y="0" width="58" height="58" rx="40"></rect><circle stroke="#FFFFFF" stroke-width="2" cx="29" cy="29" r="26.88"></circle><path d="M34.52496,40.79136 C34.36144,38.98592 34.42416,37.72592 34.42416,36.07616 C35.24176,35.6472 36.70672,32.91216 36.95424,30.6016 C37.59712,30.54896 38.61072,29.92176 38.90752,27.44544 C39.06768,26.116 38.43152,25.36784 38.044,25.13264 C39.09008,21.98656 41.26288,12.25376 34.02544,11.248 C33.28064,9.93984 31.37328,9.27792 28.89472,9.27792 C18.97824,9.46048 17.78208,16.76624 19.956,25.13264 C19.5696,25.36784 18.93344,26.116 19.09248,27.44544 C19.3904,29.92176 20.40288,30.54896 21.04576,30.6016 C21.29216,32.91104 22.81536,35.6472 23.6352,36.07616 C23.6352,37.72592 23.6968,38.98592 23.53328,40.79136 C22.12096,44.58816 14.86784,44.88496 10.68352,48.54624 C15.05824,52.9512 22.14784,56.10176 29.62944,56.10176 C37.11104,56.10176 45.90528,50.19488 47.36912,48.5832 C43.21056,44.88832 35.94064,44.6016 34.52496,40.79136 L34.52496,40.79136 Z" fill="#FFFFFF"></path></g></svg>',
+
+				init: function() {
+
+					var self = this;
+					graph.addClass('PACustomGenderCounter');
+
+					self.structure();
+
+				},
+
+				structure:function() {
+					var self = this;
+
+					var dataContainer = d3.selectAll(graph.get()).append('div')
+						.classed('PAdata', true);
+
+					var pMale = dataContainer.append('p');
+					pMale.html(self.male_icon)
+						.append('span')
+						.attr('data-value', 0)
+						.html(Number(0).format(settings.main.format));
+
+					var pFemale = dataContainer.append('p');
+					pFemale.html(self.female_icon)
+						.append('span')
+						.attr('data-value', 0)
+						.html(Number(0).format(settings.compare.format));
+
+					pMale.select('rect').style('fill', settings.main.color);
+					pFemale.select('rect').style('fill', settings.compare.color);
+				},
+
+				animate:function(data) {
+					graph.find('div.PAdata p:eq(0) > span').attr('data-value', data[0].value);
+					graph.find('div.PAdata p:eq(1) > span').attr('data-value', data[1].value);
+
+					animateNumber(graph.find('span[data-value]'), 1000, settings.main.format, 100, true);
+				}
+			},
+
 			gender_distribution_v1: {
 
 				female_icon: '<svg viewBox="0 0 58 58"><g fill="none"><rect fill="#808F96" x="0" y="0" width="58" height="58" rx="40"></rect><circle stroke="#FFFFFF" stroke-width="2" cx="29" cy="29" r="26.88"></circle><path d="M34.52496,40.79136 C34.49584,40.46992 34.47456,39.9536 34.45776,39.41824 C39.32416,38.91984 42.73792,37.7528 42.73792,36.39312 C42.72448,36.39088 42.7256,36.33712 42.7256,36.31472 C39.08784,33.03648 45.87952,9.73936 33.23584,10.212 C32.44176,9.54 31.05184,8.94304 29.05824,8.94304 C11.93232,10.23888 19.50464,32.23904 15.60256,36.392 C15.60032,36.39312 15.59696,36.39312 15.59472,36.39312 C15.59472,36.39536 15.59584,36.3976 15.59584,36.39984 C15.59584,36.40096 15.59472,36.40208 15.59472,36.40208 C15.59472,36.40208 15.59584,36.40208 15.59696,36.4032 C15.61264,37.73488 18.91104,38.88064 23.63632,39.39136 C23.62288,39.71616 23.59488,40.11824 23.53328,40.79136 C22.09744,44.652 14.62032,44.89392 10.4752,48.7344 C12.74096,50.71232 20.63584,56.02336 29.10528,56.02336 C37.57472,56.02336 44.60832,52.00368 47.4128,48.6224 C43.25872,44.89056 35.94624,44.61392 34.52496,40.79136 L34.52496,40.79136 Z" fill="#FFFFFF"></path></g></svg>',
@@ -2459,26 +2500,28 @@
 					// donut
 					var τ = 2 * Math.PI;
 					var arc = d3.svg.arc()
-						.innerRadius(65)
-						.outerRadius(65)
+						.innerRadius(graph.height() / 2)
+						.outerRadius(graph.height() / 2)
 						.startAngle(0);
 					var svg = self.donutContainer.append("svg")
-						.attr("width", 150)
-						.attr("height", 150)
+						.attr("width", graph.height())
+						.attr("height", graph.height())
 						.append("g")
-						.attr("transform", "translate(" + 75 + "," + 75 + ")")
+						.attr('width', graph.height())
+						.attr('height', graph.height())
+						.attr("transform", "translate(" + 0 + "," + 0 + ")")
 
 					var background = svg.append("path")
 						.datum({endAngle: τ})
 						.style('stroke', settings.main.color)
-						.style('stroke-width', 15)
+						.style('stroke-width', 10)
 						.attr("d", arc)
 
 					// Add the foreground arc in orange, currently showing 12.7%.
 					self.donutForeground = svg.append("path")
 						.datum({endAngle: 0 })
 						.style('stroke', settings.compare.color)
-						.style('stroke-width', 15)
+						.style('stroke-width', 10)
 						.style('opacity', 0)
 						.style('stroke-linejoin', 'round')
 						.attr("d", arc);
@@ -2496,8 +2539,8 @@
 
 					var τ = 2 * Math.PI;
 					var arc = d3.svg.arc()
-						.innerRadius(65)
-						.outerRadius(65)
+						.innerRadius(graph.height() / 2)
+						.outerRadius(graph.height() / 2)
 						.startAngle(0);
 
 					self.donutForeground.style('opacity', 1)
@@ -2686,6 +2729,171 @@
 
 				}
 
+			},
+
+			age_and_gender_distribution: {
+
+				male_icon: '<svg viewBox="0 0 17 15"><path d="M14.0801524,14.4567109 C15.7050178,12.9316006 16.7199993,10.764331 16.7199993,8.35999966 C16.7199993,3.74289934 12.9771,0 8.35999966,0 C3.74289934,0 0,3.74289934 0,8.35999966 C0,10.7678635 1.01796621,12.9379685 2.64701267,14.4634289 C2.65246531,14.4552179 2.65791339,14.4471119 2.66335656,14.4391127 C3.96472984,13.3004111 6.22053641,13.2081028 6.65978473,12.0272528 C6.71064139,11.4657395 6.69148306,11.0738645 6.69148306,10.5607696 C6.43650307,10.4273579 5.96276976,9.57637961 5.88613642,8.8581163 C5.6861931,8.84174464 5.37129978,8.64667798 5.27864312,7.87651301 C5.22917979,7.46304136 5.42703311,7.2303547 5.54720811,7.15720471 C4.87109313,4.55515481 5.24311312,2.28297657 8.32725632,2.22619824 C9.09811796,2.22619824 9.6913296,2.43206323 9.92297126,2.83891655 C12.1739012,3.15171987 11.4981345,6.17873641 11.1727912,7.15720471 C11.2933145,7.2303547 11.4911679,7.46304136 11.4413562,7.87651301 C11.3490479,8.64667798 11.0338062,8.84174464 10.8338629,8.8581163 C10.7568812,9.57672794 10.3012612,10.4273579 10.0469779,10.5607696 C10.0469779,11.0738645 10.0274713,11.4657395 10.0783279,12.0272528 C10.5186212,13.2122828 12.7796528,13.3014561 14.0730144,14.4506077 C14.0753917,14.4526194 14.077771,14.4546538 14.0801524,14.4567109 Z"></path></svg>',
+				female_icon: '<svg viewBox="0 0 17 15"><path d="M14.0799816,14.4559844 C15.7049466,12.9309593 16.7199993,10.7637549 16.7199993,8.35948679 C16.7199993,3.74266972 12.9771,0 8.35999966,0 C3.74289934,0 0,3.74266972 0,8.35948679 C0,10.7645778 1.0157477,12.9324428 2.64168654,14.4575501 C3.93892102,13.3005812 6.21815684,13.2138769 6.65978473,12.026515 C6.67894306,11.8171795 6.68765139,11.6921355 6.69183139,11.5911251 C5.22221312,11.4322948 4.19637149,11.0759717 4.19323649,10.6583457 C5.40682978,9.36680495 3.05174821,2.52491332 8.37811299,2.1219164 C8.9981463,2.1219164 9.43042795,2.30756667 9.67739627,2.51655384 C13.6097311,2.36956619 11.4974379,9.61480306 12.6326561,10.658694 C12.6326561,11.0815447 11.5709362,11.4444857 10.0574279,11.5994845 C10.0626529,11.7659777 10.0692713,11.9265495 10.0783279,12.026515 C10.5196091,13.2132765 12.7868802,13.3010401 14.0799816,14.4559844 Z"></path></svg>',
+
+				init: function() {
+
+					var self = this;
+
+					graph.addClass('PACustomAgeDist');
+
+					var ul = d3.selectAll(graph.get()).append('ul');
+
+					// detail male/female data
+					var detail = d3.selectAll(graph.get()).append('div').classed('PAdetail hide', true);
+					var detailM = detail.append('p').classed('PAmale', true).html(self.male_icon).style('color', settings.main.color);
+					var datailMT = detailM.append('label')
+					var detailF = detail.append('p').classed('PAfemale', true).html(self.female_icon).style('color', settings.compare.color);
+					var detailFT = detailF.append('label')
+
+					// params to zoom on the data
+					var max = 0, min = 100;
+					for (var i in settings.data) {
+						var v = settings.data[i].value.m + settings.data[i].value.f;
+						max = v > max ? v : max;
+						min = v < min ? v : min;
+					}
+					var coeff = Math.abs(max - min) / 50;
+
+					for (var i in settings.data) {
+
+						var li = ul.append('li')
+							.attr('data-male', settings.data[i].value.m)
+							.attr('data-female', settings.data[i].value.f)
+						var perc = settings.data[i].value.m + settings.data[i].value.f;
+						if (perc > 0) {
+
+							// label
+							li.append('label')
+								.text(settings.data[i].label)
+
+							// bar
+							var bar  = li.append('div').append('span')
+								.attr('data-perc', perc / coeff)
+								.style('background', settings.main.color)
+
+							// female bar
+							var percf = settings.data[i].value.f / (perc);
+							bar.append('span')
+								.classed('PAhide', true)
+								.attr('data-perc', percf * 100)
+								.style('background', settings.compare.color)
+
+							// value
+							li.append('span').html(Number(perc).format(settings.main.format))
+								.attr('data-value', perc);
+
+						}
+
+					}
+
+					graph.on('click', 'li', function() {
+
+						var fbar = $(this).find('span > span');
+						graph.find('li span > span').not(fbar).addClass('PAhide');
+						fbar.toggleClass('PAhide');
+
+						graph.find('div.PAdetail').toggleClass('hide', graph.find('li span > span:not(.PAhide)').length == 0)
+
+						var m = $(this).attr('data-male') * 1
+						var f = $(this).attr('data-female') * 1
+						var percm = m / (m+f) * 100;
+
+						datailMT.attr('data-value', percm);
+						detailFT.attr('data-value', 100 - percm);
+
+						animateNumber(graph.find('div.PAdetail label[data-value]'), 1000, settings.main.format, null , true);
+
+					})
+
+				},
+
+				animate: function(data) {
+
+					// params to zoom on the data
+					var max = 0, min = 100;
+					for (var i in data) {
+						var v = data[i].value.m + data[i].value.f;
+						max = v > max ? v : max;
+						min = v < min ? v : min;
+					}
+					var coeff = Math.abs(max - min) / 50;
+
+					// hide unused rows
+					d3.selectAll(graph.get()).selectAll('li:nth-child(n+'+ parseInt(data.length+1) +')')
+						.transition()
+						.duration(300)
+						.style('opacity', '0')
+						.delay(300)
+						.remove();
+
+					var lis = d3.selectAll(graph.get()).selectAll('ul li');
+
+					for (var i in data) {
+
+						var element = d3.select(lis[0][i]);
+						var perc = data[i].value.m + data[i].value.f;
+
+						// create missing rows
+						if (!element[0][0]) {
+
+							var element = d3.selectAll(graph.get()).select('ul').append('li')
+
+							if (perc > 0) {
+
+								// label
+								element.append('label')
+
+								// bar
+								var bar  = element.append('div').append('span').style('background', settings.main.color)
+
+								// female bar
+								var percf = data[i].value.f / (data[i].value.m + data[i].value.f);
+								bar.append('span')
+									.classed('PAhide', true)
+									.style('background', settings.compare.color)
+
+								// value
+								element.append('span').html(Number(perc).format(settings.main.format))
+
+							}
+
+						}
+
+						element
+							.attr('data-male', data[i].value.m)
+							.attr('data-female', data[i].value.f)
+
+						element
+							.select('label').text(data[i].label);
+
+						var percf = data[i].value.f / (perc)  * 100;
+						element
+							.select('div > span').attr('data-perc', perc / coeff)
+							.select('span')
+							.classed('PAhide', true)
+							.attr('data-perc', percf)
+
+						element
+							.select('span[data-value]').attr('data-value', perc);
+
+						graph.find('div.PAdetail').addClass('hide');
+
+
+					}
+
+
+					graph.find('span[data-perc]').each(function() {
+						$(this).width(parseInt($(this).attr('data-perc'))+'%')
+					});
+					animateNumber(graph.find('span[data-value]'), 1000, settings.main.format, null, true);
+
+				}
 			}
 
 
